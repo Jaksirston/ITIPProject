1987dae04796e9e7c4dfae667ba6276c2d98fa53
Jaksirston <13sobol13@gmail.com>
2023-05-04 23:05:30 +0300

    added Django, moderating site

--- lab1.py
+++ lab1.py
@@ -32,6 +32,21 @@ groupmates = [
     "Name": "Аркадий",
     "Surname": "Гусевешвилли",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
-    "Marks": [3, 4, 5, 5]
+    "Marks": [5, 4, 5, 5]
     },
 ]
+def print_students(students):
+    print(u"Имя".ljust(12), u"Фамилия".ljust(12), u"Экзамены".ljust(43), u"Оценки".ljust(20), u"средняя оценка".ljust(20))
+    for student in students:
+        print(student["name"].ljust(12), student["surname"].ljust(12), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20), str(student["sredn"]).ljust(20))
+def searchSredn():
+    poisk = float(input("средняя оценка"))
+    new_group = []
+    for students in groupmates:
+        sredn = sum(students['marks'])/len(students['marks'])
+        if(sredn > poisk):
+            students['sredn'] = sredn
+            new_group.append(students)
+    print_students(new_group)
+
+searchSredn()
