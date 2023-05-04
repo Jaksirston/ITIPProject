f6e053d7d15c23679375cbd8cb9f840103fc5656
Jaksirston <13sobol13@gmail.com>
2023-05-04 20:10:31 +0300

    123

--- lab1.py
+++ lab1.py
@@ -4,30 +4,34 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 groupmates = [
-{
+    {
     "Name": "Илья",
     "Surname": "Антоненко",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [3, 2, 5, 3]
-},
-{
+    },
+    {
     "Name": "Иван",
     "Surname": "Человеков",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [4, 5, 5, 4]
-},
-{
+    },
+    {
     "Name": "Константин",
     "Surname": "Держанов",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [4, 3, 4, 4]
-},
-{
+    },
+    {
     "Name": "Полина",
     "Surname": "Вершкова",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [5, 4, 3, 4]
-}
+    },
+    {
+    "Name": "Аркадий",
+    "Surname": "Гусевешвилли",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [3, 4, 5, 5]
+    },
 ]
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
