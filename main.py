8d67f2ed1fdbd94de60f4b5f3925f09ab52518f1
Соболькина Полина <13sobol13@gmail.com>
2023-05-04 19:55:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+groupmates = [
+{
+    "Name": "Илья",
+    "Surname": "Антоненко",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [3, 2, 5, 3]
+},
+{
+    "Name": "Иван",
+    "Surname": "Человеков",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [4, 5, 5, 4]
+},
+{
+    "Name": "Константин",
+    "Surname": "Держанов",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [4, 3, 4, 4]
+},
+{
+    "Name": "Полина",
+    "Surname": "Вершкова",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [5, 4, 3, 4]
+}
+]
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
