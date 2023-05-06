4ae74ae516d433ce36861845afa219c309123974
Jaksirston <13sobol13@gmail.com>
2023-05-06 23:42:54 +0300

    fixed 1-part 1-lab

--- lab1.py
+++ lab1.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 groupmates = [
     {
     "Name": "Илья",
@@ -36,17 +31,17 @@ groupmates = [
     },
 ]
 def print_students(students):
-    print(u"Имя".ljust(12), u"Фамилия".ljust(12), u"Экзамены".ljust(43), u"Оценки".ljust(20), u"средняя оценка".ljust(20))
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(43), u"Оценки".ljust(20), u"средняя оценка".ljust(20))
     for student in students:
-        print(student["name"].ljust(12), student["surname"].ljust(12), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20), str(student["sredn"]).ljust(20))
-def searchSredn():
-    poisk = float(input("средняя оценка"))
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20), str(student["middle"]).ljust(20))
+def searchMiddle():
+    search = float(input("средняя оценка"))
     new_group = []
     for students in groupmates:
-        sredn = sum(students['marks'])/len(students['marks'])
-        if(sredn > poisk):
-            students['sredn'] = sredn
+        middle = sum(students['marks'])/len(students['marks'])
+        if(middle > search):
+            students['middle'] = middle
             new_group.append(students)
     print_students(new_group)
 
-searchSredn()
+searchMiddle()
--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 groupmates = [
-{
+    {
     "Name": "Илья",
     "Surname": "Антоненко",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [3, 2, 5, 3]
-},
-{
+    },
+    {
     "Name": "Иван",
     "Surname": "Человеков",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [4, 5, 5, 4]
-},
-{
+    },
+    {
     "Name": "Константин",
     "Surname": "Держанов",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [4, 3, 4, 4]
-},
-{
+    },
+    {
     "Name": "Полина",
     "Surname": "Вершкова",
     "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
     "Marks": [5, 4, 3, 4]
-}
+    },
+    {
+    "Name": "Аркадий",
+    "Surname": "Гусевешвилли",
+    "Exams": ["Метрология", "ОБЖ", "ИТИП", "МИСПИСИТ"],
+    "Marks": [5, 4, 5, 5]
+    },
 ]
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(43), u"Оценки".ljust(20), u"средняя оценка".ljust(20))
+    for student in students:
+        print(student["Name"].ljust(15), student["Surname"].ljust(10), str(student["Exams"]).ljust(30), str(student["Marks"]).ljust(20), str(student["Middle"]).ljust(20))
+def searchMiddle(groupmates):
+    search = float(input("средняя оценка"))
+    new_group = []
+    for students in groupmates:
+        middle = sum(students['Marks'])/len(students['Marks'])
+        if(middle > search):
+            students['Middle'] = middle
+            new_group.append(students)
+    print_students(new_group)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+searchMiddle(groupmates)
